503167f00f37ff63cf2f6a4fbdaa3171371ad48d
yasmin <yasmin@yasminguimaraes.com>
2025-07-25 13:53:27 -0300

    adjust code

--- main.go
+++ main.go
@@ -5,29 +5,33 @@ import (
 	"sync"
 )
 
-var counter int
-var mutex sync.Mutex
+type SyncCounter struct {
+	sync.Mutex
+	Value int
+}
+
+var counter SyncCounter
 
 func main() {
 	var wg sync.WaitGroup
 
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		// opening a new goroutine to increment the counter variable that is global
-		go incremental(&wg)
+		// opening a new goroutine to increment the counter variable that is shared
+		go incremental(&counter, &wg)
 	}
 
 	wg.Wait()
-	fmt.Println("Final counter:", counter)
+	fmt.Println("Final counter:", counter.Value)
 }
 
-func incremental(wg *sync.WaitGroup) {
+func incremental(syncCounter *SyncCounter, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for i := 0; i < 1000; i++ {
-		mutex.Lock()
+		//syncCounter.Lock()
 		// lock before add a value to the counter
-		counter++
+		syncCounter.Value++
 		// unlock after the operation being done
-		mutex.Unlock()
+		//syncCounter.Unlock()
 	}
 }
